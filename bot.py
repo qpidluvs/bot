fa2ec499d654f867dd07a5d3a0f0f66b12cf05b8
qpidluvs <160605639+qpidluvs@users.noreply.github.com>
2025-07-16 14:27:36 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,9 +4,6 @@ from discord.ext import commands
 from discord import app_commands, ui, Embed, Interaction, ButtonStyle
 from datetime import datetime
 import os
-token = os.getenv("DISCORD_TOKEN")
-print(f"Token loaded? {'Yes' if token else 'No'}")
-bot.run(token)
 
 intents = discord.Intents.default()
 intents.members = True
@@ -298,5 +295,6 @@ class ChannelSelectView(ui.View):
 async def postembed(interaction: discord.Interaction):
     await interaction.response.send_modal(EmbedModal())
 
-import os
-bot.run(os.getenv("DISCORD_TOKEN"))
+token = os.getenv("DISCORD_TOKEN")
+print(f"Token loaded? {'Yes' if token else 'No'}")
+bot.run(token)
